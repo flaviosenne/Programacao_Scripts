728d2fac5a19c601a752cc7993788df5afe51746
Joao flavio <flaviosenne123@gmail.com>
2021-04-03 20:23:47 -0300

    login and register finished

--- client/src/main.js
+++ client/src/main.js
@@ -10,8 +10,6 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
-//temporário
-require('axios').defaults.headers.common['Authorization']='bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwibmFtZSI6ImpvYW9mbGF2aW8iLCJlbWFpbCI6ImZsYXZpb0BlbWFpbC5jb20iLCJhZG1pbiI6MSwiaWF0IjoxNjE3NDg4MzA3LCJleHAiOjE2MTc3NDc1MDd9.oN8vFcHnxecysHOodueagRMOpsvyoldSfeY00kcX4SI'
 new Vue({
   store,
   router,
